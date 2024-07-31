3c8ef3d553fa967c5f215c2b1dcbcc8a240eb00e
arl0399 <aripermana2606@gmail.com>
2024-07-31 11:06:32 +0000

    update

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,17 +1,17 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("IzzyToken","IZZY"){} 
+    constructor()ERC20("Swisstronik","SWTR"){} 
 
-    function mint1000tokens() public {
-        _mint(msg.sender,1000*10**18);
+    function mint100tokens() public {
+        _mint(msg.sender,100*10**18);
     }
 
-    function burn1000tokens() public{
-        _burn(msg.sender,1000*10**18);
+    function burn100tokens() public{
+        _burn(msg.sender,100*10**18);
     }
     
 }
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = ''
+const deployedAddress = '0x38ac397491c01047e472439Ee86a70229106fd67'
 
 export default deployedAddress
